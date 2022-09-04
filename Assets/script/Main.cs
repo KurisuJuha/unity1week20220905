5127394184a9cfda3ad4893563b608a439935ddc
KYapp <kukiyomenaisapp@gmail.com>
2022-09-05 00:35:19 +0900

    ディスプレイ表示までできた

--- Assets/script/Main.cs
+++ Assets/script/Main.cs
@@ -1,16 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Unixel.Unity;
+using Unixel.Core;
+using Unixel.Core.Input;
 
-public class Main : MonoBehaviour
+public class Main
 {
-    void Start()
+    public UnixelCore Core;
+    public UnixelInput Input;
+
+    [UnityEngine.RuntimeInitializeOnLoadMethod(UnityEngine.RuntimeInitializeLoadType.AfterSceneLoad)]
+    public static void Init()
     {
-        
+        var s = new Main();
+
+        UnixelUnity.Init(256, 256);
+        s.Core = UnixelUnity.core;
+        s.Input = s.Core.Input;
+        UnixelUnity.core.Start += s.Start;
+        UnixelUnity.core.Update += s.Update;
     }
 
-    void Update()
+    public void Start()
     {
-        
+
+    }
+
+    public void Update()
+    {
+
     }
 }
